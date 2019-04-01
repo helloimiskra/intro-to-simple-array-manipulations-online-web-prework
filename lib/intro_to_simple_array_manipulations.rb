4b3bdf39bc0b0a153f1c2c36bf935f3623589d90
Iskra B <ibatistapoblete@gmail.com>
2019-04-01 14:51:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -32,7 +32,7 @@ def using_concat(array_1, array_2)
 end
 
 def using_insert(array, element)
-  array.insert(3, 'another_esoteric_language')
+  array.insert(4, 'another_esoteric_language')
 end
 
 def using_uniq(array)
