f206dc4da76b28388c2cc9eb83b3f73ddba0279b
Iskra B <ibatistapoblete@gmail.com>
2019-04-01 14:29:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,6 +1,41 @@
-def using_push (countries_in_western_africa)
+def using_push (countries_in_western_africa, next_country)
   countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
   next_country = "Niger"
   countries_in_western_africa.push(next_country)
   return
 end
+
+
+def using_unshift
+end
+
+def using_pop
+end
+
+def pop
+end
+
+def using_shift
+end
+
+def shift_with_args
+end
+
+def using_concat
+end
+
+def using_insert
+end
+
+def using_uniq
+end
+
+def using_flatten
+end
+
+def using_delete
+end
+
+def using_delete_at
+end
+  
