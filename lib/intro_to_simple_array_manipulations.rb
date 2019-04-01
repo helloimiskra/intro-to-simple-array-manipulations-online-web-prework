a1f4d1ef16c93b3a2452a3f34889f6b905038406
Iskra B <ibatistapoblete@gmail.com>
2019-04-01 14:50:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -31,8 +31,8 @@ def using_concat(array_1, array_2)
   array_1.concat(array_2)
 end
 
-def using_insert(array, 'Malbolge')
-  array.insert(4, 'Malbolge')
+def using_insert(array, element)
+  array.insert(3, element)
 end
 
 def using_uniq(array)
