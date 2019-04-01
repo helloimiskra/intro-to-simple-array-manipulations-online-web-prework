e1a71cd56e79c72fd6d99b724eff8b16597ee816
Iskra B <ibatistapoblete@gmail.com>
2019-04-01 14:42:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,13 +15,16 @@ def using_pop(array)
   array.pop
 end
 
-def pop
+def pop_with_args(array)
+  array.pop(2)
 end
 
-def using_shift
+def using_shift(array)
+  array.shift
 end
 
-def shift_with_args
+def shift_with_args(array)
+  array.shift(2)
 end
 
 def using_concat
